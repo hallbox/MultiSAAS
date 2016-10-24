7285b16a64ec3dd1487e6a9d1f34a5e941722b16
Matt Hall <mhall@rt-systems.com>
2016-10-23 20:23:05 -0400

    navbar

--- MultiSAAS.Web/Framework/Helpers/BootstrapHelpers.cs
+++ MultiSAAS.Web/Framework/Helpers/BootstrapHelpers.cs
@@ -1,10 +1,12 @@
 ﻿namespace MultiSAAS.Web.Framework.Helpers
 {
   using System;
+  using System.Linq;
   using System.Linq.Expressions;
   using System.Web;
   using System.Web.Mvc;
   using System.Web.Mvc.Html;
+  using System.Web.Routing;
 
   public static class BootstrapHelpers
   {
@@ -24,6 +26,31 @@
       {
         @class = LabelCssClass
       });
+    }
+
+    public static string IsActive(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "active")
+    {
+      ViewContext viewContext = html.ViewContext;
+      bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+      if (isChildAction)
+        viewContext = html.ViewContext.ParentActionViewContext;
+
+      RouteValueDictionary routeValues = viewContext.RouteData.Values;
+      string currentAction = routeValues["action"].ToString();
+      string currentController = routeValues["controller"].ToString();
+
+      if (String.IsNullOrEmpty(actions))
+        actions = currentAction;
+
+      if (String.IsNullOrEmpty(controllers))
+        controllers = currentController;
+
+      string[] acceptedActions = actions.Trim().Split(',').Distinct().ToArray();
+      string[] acceptedControllers = controllers.Trim().Split(',').Distinct().ToArray();
+
+      return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+          cssClass : String.Empty;
     }
   }
 }
